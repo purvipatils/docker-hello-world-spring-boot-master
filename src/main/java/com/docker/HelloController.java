f789db5086d9b33116784d1e157cc774ca38f27c
purvipatils <asmitapatilweb@gmail.com>
2022-07-30 14:25:49 +0530

    file

---
+++ src/main/java/com/docker/HelloController.java
@@ -0,0 +1,15 @@
+package com.docker;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello Docker\n";
+    }
+    
+}
