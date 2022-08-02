535ed3e9d8424e63efce9c85e8a562081363162b
purvipatils <asmitapatilweb@gmail.com>
2022-08-02 14:42:09 +0530

    EKL file

--- src/main/java/com/docker/Application.java
+++ src/main/java/com/docker/Application.java
@@ -1,6 +1,5 @@
 package com.docker;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
